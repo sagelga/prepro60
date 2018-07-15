546070f9f5f3cf8982953f23ebe3d7a58df35e8f
son9912 <son9912@gmail.com>
2018-07-16 00:14:56 +0700

    Removed all space character from file name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
 
         // GitHub edit link
         repo: 'sagelga/PreProgramming-60',
-        repoLabel: 'PrePro60 Repository',
+        repoLabel: 'PrePro60 Repo',
         docsRepo: 'sagelga/PreProgramming-60',
         docsDir: 'docs',
         editLinks: true,
@@ -21,23 +21,41 @@ module.exports = {
 
         // Navigation Bar
         nav: [
-            { text: 'Report a Problem', link: 'https://github.com/sagelga/PreProgramming-60/issues' },
+            { text: 'Python Cheatsheet', link: 'https://sagelga.github.io/cheatsheet/#/Python/' },
+            { text: 'Report a Problem', link: 'https://github.com/sagelga/PreProgramming-60/issues' }
+
         ],
 
         // Sidebar
         sidebar: [
             {
-                title: 'Lecture Cheatsheet',
-                collapsable: false,
+                title: 'Online Question',
+                collapsable: true,
                 children: [
-                  '/cheatsheet/Introduction/',
-                    '/cheatsheet/IO/',
-                    '/cheatsheet/Function/',
-                    '/cheatsheet/String/',
-                    '/cheatsheet/Loop/',
-                    '/cheatsheet/Condition/',
-                    '/cheatsheet/List/',
-                    '/cheatsheet/Dictionary/',
+                    'Online-Question/Week1/Special-ITCAMP13.md',	
+                    'Online-Question/Week1/Absolute.md',
+                    'Online-Question/Week1/Discounted.md',
+                    'Online-Question/Week1/Distance.md',
+                    'Online-Question/Week1/Echo.md',
+                    'Online-Question/Week1/FirstImpression.md',
+                    'Online-Question/Week1/IO.md',
+                    'Online-Question/Week1/MathematicianV2.md',
+                    'Online-Question/Week1/Mathematician.md',
+                    'Online-Question/Week1/MaxMeanMin.md',
+                    'Online-Question/Week1/PersonalInformation.md',
+                    'Online-Question/Week1/SecretCode.md',
+                    'Online-Question/Week1/SquareRoot.md',
+                    'Online-Question/Week1/ToInteger.md',
+                    'Online-Question/Week1/TooManyLines.md',
+                    'Online-Question/Week1/GroupUpWithMe.md',
+                    'Online-Question/Week1/Hardcore-BankNotes.md',
+                    'Online-Question/Week1/Hardcore-Correlated.md',
+                    'Online-Question/Week1/Hardcore-CramersRule.md',
+                    'Online-Question/Week1/Hardcore-EQUALLYDistributed.md',
+                    'Online-Question/Week1/Hardcore-Grading.md',
+                    'Online-Question/Week1/Hardcore-Sequence.md',
+                    'Online-Question/Week1/Hardcore-TaxNotIncluded.md',
+                    'Online-Question/Week1/Hardcore-WeirdThatsOdd.md'
                 ]
             },
 
